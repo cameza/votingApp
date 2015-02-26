f301f1dd4d50c98c99d59edd79ef2d23498c8f0a
cameza <camilo.meza@me.com>
2015-02-26 21:14:24 +0000

    added first tests in create_spec.rv

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 VotingApp::Application.routes.draw do
+  resources :nominees
+  root 'nominees#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
